310bba696c534f731607b22bb0e0d3a9672151bb
Shakil Ahmed <92939397+sa3akash@users.noreply.github.com>
2022-09-26 22:28:41 +0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
+add file in to server.js in crating server
